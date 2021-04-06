sifive/riscv-binutils-gdb

RISC-V: Clarify the supported versions for the unratified extensions.
Remove the V-ext v0.7 in the riscv_ext_version_table, and regard V-ext
as a non-ISA spec extension temporarily.  Since we only support the v1.0
for now, and not yet upstreamed.  Also, we define other unratified
extensions to the non-ISA spec extension, too.

gas/testsuite/gas/riscv/attribute-15-unratified.d
@@ -1,0 +1,6 @@
+#as: -march-attr -march=rv32ifv_zfh_zvamo_zvediv_zvlsseg_zvqmac
+#readelf: -A
+#source: empty.s
+Attribute Section: riscv
+File Attributes
+  Tag_RISCV_arch: ".*_v1p0_zfh0p1_zvamo1p0_zvediv1p0_zvlsseg1p0_zvqmac1p0"

XUANTIE-RV/binutils-gdb

gas/testsuite/gas/riscv/attribute-15-unratified.d
@@ -1,0 +1,6 @@
+#as: -march-attr -march=rv32ifv_zvamo_zvediv_zvlsseg_zvqmac
+#readelf: -A
+#source: empty.s
+Attribute Section: riscv
+File Attributes
+  Tag_RISCV_arch: ".*_v1p0_zvamo1p0_zvediv1p0_zvlsseg1p0_zvqmac1p0"